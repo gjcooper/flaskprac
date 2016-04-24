448542ecc214c07213f4d6d640122510de612d01
Gavin Cooper <gjcooper@gmail.com>
2016-04-24 15:51:03 +1000

    Templating part 1

--- app/views.py
+++ app/views.py
@@ -1,6 +1,20 @@
+from flask import render_template
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello World!'
+    user = {'nickname': 'Gavin'}
+    posts = [
+        {
+            'author': {'nickname': 'Coop'},
+            'body': 'I totally love this guy man!'
+        },
+        {
+            'author': {'nickname': 'Lish'},
+            'body': 'I totally love my main man!'
+        }
+    ]
+
+    return render_template('index.html', title='Base', user=user, posts=posts)
