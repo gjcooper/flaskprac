95205818c73b3e3851512445339eae4da74e5c9f
Gavin Cooper <gjcooper@gmail.com>
2016-04-26 22:24:04 +1000

    Ignore updated, db manipulation scripts

--- app/models.py
+++ app/models.py
@@ -5,8 +5,19 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nickname = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(128), index=True, unique=True)
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self):
         """How obijects of the class are printed"""
         return '<User {}>'.format(self.nickname)
         
+class Post(db.Model):
+    """Represents a content post by one author"""
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        """How posts are printed to the screen"""
+        return '<Post {}>'.format(self.body)
---
+++ db_create.py
@@ -0,0 +1,13 @@
+#!venv/bin/python
+from migrate.versioning import api
+from config import SQLALCHEMY_MIGRATE_REPO, SQLALCHEMY_DATABASE_URI
+from app import db
+import os.path
+db.create_all()
+if os.path.exists(SQLALCHEMY_MIGRATE_REPO):
+    api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO,
+                        api.version(SQLALCHEMY_MIGRATE_REPO))
+else:
+    api.create(SQLALCHEMY_MIGRATE_REPO, 'database repository')
+    api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
+
