257afd773cb4b1e323880dfa509f36c065c60d08
Gavin Cooper <gjcooper@gmail.com>
2016-05-17 21:29:52 +1000

    Repeat of previous

--- app/views.py
+++ app/views.py
@@ -111,3 +111,12 @@ def edit():
         form.nickname.data = g.user.nickname
         form.aboutme.data = g.user.aboutme
     return render_template('edit.html', form=form)
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('500.html'), 500
